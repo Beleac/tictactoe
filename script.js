50b07d08b864aa384f353037f5b6e67c1a0b8ccb
Student <student@Hermosas-MacBook-Air-7.local>
2019-07-19 15:03:53 -0700

    Added X and O placement arrays

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var $td= $('td')
 var xoro= "X"
 var clickedBoxes = [];
+var XPlacement = [];
+var OPlacement = [];
+
 console.log($td)
 
    
@@ -14,23 +17,23 @@ console.log($td)
    clickedBoxes.push(tableId);
 
    console.log(clickedBoxes);
-  
 
    this.innerHTML = xoro
-
-   // alert(tableId + xoro)
-
+  
    if(xoro == "X")
    {
+   XPlacement.push(tableId)
    xoro = "O"
    }
 
    else if (xoro == "O")
    {
+   OPlacement.push(tableId)
    xoro = "X"
    }
 
    }
+
 
    });
 
